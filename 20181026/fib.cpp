28bf274bc325b8ba95323781c8c0bdf0aa1ef871
terrellhu <terrellhu@tencent.com>
2018-10-26 16:07:27 +0800

    fibex

--- 20181026/fib.cpp
+++ 20181026/fib.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
+
+map<uint64_t, uint64_t> g_mapFib;
 
 uint64_t Fib(uint64_t n)
 {
     if (n<=1) return n;
     return Fib(n-1)+Fib(n-2);
+}
+
+//用MAP来存已经计算过的值，不再重复计算
+uint64_t Fibex(uint64_t n)
+{
+    if (n<=1){
+        return n;
+    }
+    uint64_t t = g_mapFib[n];
+    if (0 == t){
+        t = Fibex(n-2) + Fibex(n-1);
+        g_mapFib[n] = t;
+    }
+    
+    return t;
 }
 
 int main(int argc, char** argv)

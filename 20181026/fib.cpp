940855be2392da2cc7f1413a63e7045ff18a3894
terrellhu <terrellhu@tencent.com>
2018-10-26 09:17:55 +0800

    fib

---
+++ 20181026/fib.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+uint64_t Fib(uint64_t n)
+{
+    if (n<=1) return n;
+    return Fib(n-1)+Fib(n-2);
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        cerr << "proc n" << endl;
+    }
+    uint64_t n = strtoul(argv[1]);
+    cout << "fib=" << Fib(n) << endl;
+    return 0;
+}
